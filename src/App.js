94228b38406687501409122a4775c65c690f8b6d
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-07-19 10:17:32 +0600

    third

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useEffect, useState } from 'react';
+import Country from './components/Country/Country';
+import Person from './components/person/Person';
+import Header from './components/Header/Header';
    
 
 
@@ -8,24 +11,15 @@ import { useEffect, useState } from 'react';
 function App() {
   return (
     <div className="App">
-        <LoadCountries></LoadCountries>
+      <Header></Header>
+       <Country></Country>
+       <Person></Person>
     </div>
   );
 }
- function LoadCountries(){
+ 
 
-  const [countries, setCountries ] =useState([]);
-  useEffect(()=>{
-         fetch("https://restcountries.com/v3.1/all")
-         .then(res=>res.json())
-         .then(data => console.log(data))
-  }, [])
+ 
 
-  return (
-    <div>
-      <h1>Visiting Every Country of the World!!!..</h1>
-      <h3>Available Countries: { countries.length}</h3>
-    </div>
-  )
- }
+
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to My React App </h2>
+            <nav>
+                <a href="/home">Home</a>
+                <a href="/shop">Shop</a>
+                <a href="/about">About us</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
